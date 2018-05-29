1cf1761f3a229e1f48bd22c794d6e65eb9e546ba
Bludova <bludova2012@mail.ru>
2018-05-29 15:13:17 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,60 @@
+<?php
+ include_once  "include/header.html";
+ ?>
+
+  <div class="info">
+    <div class="row centered center">
+    <div class="col-lg-12 col-lg-offset-2 info-caption">
+        <h2>Наша компания КАЛИБРИ ля-ля-ля</h2>
+    </div>
+    <div class="info-nav">
+      <ul class="nav nav-pills navbar-right">
+        <li><a href="#">Пленки</a></li>
+        <li><a href="#">Цены</a></li>
+        <li><a href="#">Процесс работы</a></li>
+      </ul>
+    </div>
+<!-- <div class="tabbable">  -->
+ <!--  <ul class="nav nav-tabs navbar-centered">
+    <li class="active"><a href="#tab1" data-toggle="tab">Вкладка 1</a></li>
+    <li><a href="#tab2" data-toggle="tab">Вкладка 2</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1">
+      <p>Я первая секция.</p>
+    </div>
+    <div class="tab-pane" id="tab2">
+      <p>Привет, я 2-я секция.</p>
+    </div>
+  </div>
+</div>
+ -->
+
+
+    </div>
+    <hr>
+
+    <div class="col-lg-4">
+
+    <ul class="nav nav-list">
+      <li class="nav-header">Заголовок</li>
+      <li class="active"><a href="#">На главную</a></li>
+      <li><a href="#">Библиотека</a></li>
+    </ul>
+    </div>
+
+
+        <div class="content col-lg-8 ">
+
+           <h5>JJ</h5>
+            <p>Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+
+
+    </div>
+  </div>
+
+ <?php
+ include_once  "include/footer.html";
+ ?>
---
+++ index.php
@@ -0,0 +1,340 @@
+<?php
+ include_once  "include/header.html";
+ ?>
+
+  <div id="headerwrap">
+  <div class="container">
+
+    <div class="row centered center">
+      <div class="col-lg-8 col-lg-offset-2">
+        <div class="contacts">
+        <div class="col-sm-6 col">
+          <p>
+          Волгоград <br>ул. Домостроительная 13б
+        </p>
+        </div>
+
+        <div class="col-sm-6">
+          <p>+7981-901-60-00<br>
+          Время работы с 9:00 до 19:00
+        </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ <div class="main">
+    <div class="sl">
+
+      <div class="sl__slide">
+        <img class="sl__img" src="img/background.jpg" alt=" Картинка слайда 1">
+        <div class="sl__text">
+          <h3 class="sl__zag">Химическая металлизация</h3>
+          <p class="sl__desc">Описание слайда №1</p>
+        </div>
+      </div>
+
+       <div class="sl__slide">
+        <img class="sl__img" src="img/background2.jpg" alt=" Картинка слайда 2">
+        <div class="sl__text">
+          <h3 class="sl__zag">Аквапринт</h3>
+          <p class="sl__desc">Описание слайда №2</p>
+        </div>
+      </div>
+
+       <div class="sl__slide">
+        <img class="sl__img" src="img/background3.jpg" alt=" Картинка слайда 3">
+        <div class="sl__text">
+          <h3 class="sl__zag">Слайдер 3</h3>
+          <p class="sl__desc">Описание слайда №13</p>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
+ </div>
+
+
+<div class="container about-company">
+  <div class="row centered">
+    <br><br>
+    <div class="col-lg-12 ">
+      <h4>О компании:</h4>
+      <p>Компания КОЛИБРИ отличается умом и сообразительностью </p>
+      <p>Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="container services">
+  <div class="row centered">
+    <br><br>
+    <div class="col-lg-12">
+      <h4>Услуги</h4>
+    </div>
+
+    <div class="col-lg-12">
+      <h4>Автомобили</h4>
+    </div>
+
+    <div class="col-lg-12">
+      <h4>Мотоциклы</h4>
+    </div>
+
+    <div class="col-lg-12">
+      <h4>Диски</h4>
+    </div>
+
+    <div class="col-lg-12">
+      <h4>Оружие</h4>
+    </div>
+
+    <div class="col-lg-12">
+      <h4>Разное</h4>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+<div class="container">
+  <div class="row centered">
+    <br><br>
+    <div class="col-lg-12 advantages">
+      <h4>Почему стоит выбрать именно нас</h4>
+
+         <div class="col-lg-offset-1  col-lg-3 add-indent">
+        <!--   <img src="img/demo/ryk.jpg"> -->
+            <p><i class="fa fa-thumbs-up" aria-hidden="true"></i>Нам доверяют</p>
+        </div>
+
+        <div class="col-lg-3">
+          <!-- <img src="img/demo/ryk.jpg"> -->
+            <p><i class="fa fa-thumbs-up" aria-hidden="true"></i>Качественные материалы</p>
+        </div>
+
+        <div class="col-lg-3">
+            <p><i class="fa fa-thumbs-up" aria-hidden="true"></i>Гарантия 5 лет</p>
+        </div>
+
+        <div class="col-lg-3">
+            <p><!-- <i class="fa fa-thumbs-up" aria-hidden="true"></i> -->Высокое качество материалов</p>
+        </div>
+
+        <div class="col-lg-3">
+            <p><!-- <i class="fa fa-thumbs-up" aria-hidden="true"></i> -->Акции и гибкая система скидок</p>
+        </div>
+
+        <div class="col-lg-3">
+            <p><!-- <i class="fa fa-thumbs-up" aria-hidden="true"></i> -->Индивидуальный подход к каждому проекту</p>
+        </div>
+
+      </div>
+  </div>
+</div>
+
+
+<!--             Высокое качество материалов
+            Акции и гибкая система скидок
+            Индивидуальный подход к каждому проекту
+            Большой опыт работ любой сложности -->
+
+            <!-- КАК МЫ РАБОТАЕМ?
+            Обращение 0%
+            Обсуждение 25%
+            Концепт 50%
+            Реализация 75%
+            Готовый проект 100% -->
+
+
+
+<div class="container our-works">
+  <div class="row centered">
+    <br><br>
+    <div class="col-lg-12">
+      <h4>Наши работы:</h4>
+      <p>автомобилей, преобразившихся благодаря нам</p>
+    </div>
+
+          <div class="col-lg-3 ">
+        <div class="description">
+            <img src="img/h8.JPG">
+           <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+
+     <div class="col-lg-3 ">
+        <div class="description">
+            <img src="img/a3.jpg">
+           <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+     <div class="col-lg-3 ">
+        <div class="description">
+            <img src="img/e4.jpg">
+            <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+
+          <div class="col-lg-3">
+        <div class="description">
+            <img src="img/h8.JPG">
+            <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+
+     <div class="col-lg-3">
+        <div class="description">
+            <img src="img/a3.jpg">
+             <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+     <div class="col-lg-3">
+        <div class="description">
+            <img src="img/e4.jpg">
+             <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+     <div class="col-lg-3">
+        <div class="description">
+            <img src="img/a3.jpg">
+            <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+
+
+     <div class="col-lg-3">
+        <div class="description">
+            <img src="img/e4.jpg">
+            <h5>Вид работы</h5>
+            <p>Краткое описание</p>
+             <a href="#">Подробнее</a>
+        </div>
+     </div>
+<!-- форма обратной связи
+
+ВЫ МОЖЕТЕ ПРОСТО ОСТАВИТЬ
+СВОИ ДАННЫЕ И ВСКОРЕ МЫ ВАМ ПЕРЕЗВОНИМ
+
+
+и КАК МЫ РАБОТАЕМ
+-->
+
+<!--     <div class="col-lg-12"></div>
+    <div class="col-lg-12"></div>
+    <div class="col-lg-12"></div> -->
+  </div>
+</div>
+
+
+
+
+<div class="container reviews-clients">
+  <div class="row centered">
+    <!-- <div class="col-lg-12 caption"> -->
+      <h4>Отзывы наших клиентов:</h4>
+    <!-- </div> -->
+
+          <div class="col-lg-12">
+            <div class="col-lg-4">
+              <img src="img/images.jpg">
+            </div>
+            <div class="reviews col-lg-8">
+            <h5>Вася Пупкин</h5>
+            <p>Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <br><i>Дата</i></p>
+          </div>
+        </div>
+
+         <div class="col-lg-12">
+          <div class="col-lg-4">
+            <img src="img/images.jpg">
+          </div>
+             <div class="reviews col-lg-8">
+            <h5>Петя Пупкин</h5>
+            <p>Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br><i>Дата</i></p>
+          </div>
+        </div>
+
+
+
+     <div class="col-lg-12">
+      <div class="col-lg-4">
+        <img src="img/images.jpg">
+      </div>
+       <div class="reviews col-lg-8">
+         <h5>Игорь Пупкин</h5>
+         <p>Lorem ipsum — классическая панграмма, условный, зачастую бессмысленный текст-заполнитель, вставляемый в макет страницы. ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br><i>Дата</i></p>
+       </div>
+     </div>
+  </div>
+</div>
+
+
+
+
+
+<!--   <div id="lg">
+    <div class="container">
+      <div class="row centered">
+        <h4>ЗАЛОГ УСПЕХА НАШЕЙ РАБОТЫ</h4>
+
+        <div class="col-lg-2 col-lg-offset-2">
+          <img src="img/equipment.png">
+          <p>ПРОФЕССИОНАЛЬНАЯ ТЕХНИКА</p>
+        </div>
+
+        <div class="col-lg-2">
+          <img src="img/prices.png">
+          <p>ДОСТУПНЫЕ ЦЕНЫ</p>
+        </div>
+
+        <div class="col-lg-2">
+          <img src="img/team.png">
+          <p>ОПЫТНАЯ КОМАНДА</p>
+        </div>
+
+        <div class="col-lg-2">
+          <img src="img/terms.png">
+          <p>ТОЧНЫЕ СРОКИ</p>
+        </div>
+    <div class="col-lg-2">
+          <img src="img/equipment.png">
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+
+<?php
+ include_once  "include/footer.html";
+ ?>
